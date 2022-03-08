34a533323272e9097a76d3ae86258113366b8b0c
YohannTonnerre <yohann.tonnerre@gmail.com>
2022-03-08 17:30:36 +0100

    :lipstick: home page

--- src/components/Video/VideoMiniature.tsx
+++ src/components/Video/VideoMiniature.tsx
@@ -5,11 +5,13 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
 
 function VideoMiniature(props: any) {
     return (
-        <Link to={`/watch/${props.id}`} className='video-miniature'>
-            <img src={uploadUrl + '/miniatures/' + props.miniature} />
-            <h2>{props.title}</h2>
-            <h1>{props.views}</h1>
-        </Link>
+        <div className='video-miniature-container'>
+            <Link to={`/watch/${props.id}`} className='video-miniature'>
+                <img src={uploadUrl + '/miniatures/' + props.miniature} />
+                <h2>{props.title}</h2>
+                <h1>{props.views}</h1>
+            </Link>
+        </div>
     );
 }
 
