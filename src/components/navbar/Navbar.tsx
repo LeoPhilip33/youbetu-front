1f6928cb736e0e50abc0e8cea761039fc4d1586c
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2022-03-08 11:56:16 +0100

    test

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,15 @@
 import logo from '../../img/youbetu.png';
-import upload from '../../img/upload.png'
-import search from '../../img/search.png'
-import user from '../../img/profile.png'
-import './Navbar.css';
+import './Navbar.scss';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
 
 function Navbar() {
   return (
-      <div className='navbar'>
-            <div>
-                <img src={logo} className="logo" alt="oui" />
-            </div>
-            <div className='d-flex search-bar'>
-                <input type="text" placeholder="Rechercher" className="input-search-bar"/>
-                <img src={search} className="search" alt="Logo youbetu" />
-            </div>
-            <div className='d-flex'>
-                <div>
-                    <img src={upload} className="upload" alt="Upload vidéo" />
-                </div>
-                <div>
-                    <img src={user} className="user" alt="user" />
-                </div>
-            </div>
-      </div>
+    <div className='navbar'>
+      <div> <img src={logo} className="logo" alt="oui" /> </div>
+      <div> searchbar </div>
+      <div> importer vidéo </div>
+      <Link to="/register"> logo profile </Link>
+    </div>
   );
 }
 
