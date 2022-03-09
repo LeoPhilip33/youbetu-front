c0bbeb75b48fa646e9172b1b4edff775e8b2be6d
YohannTonnerre <yohann.tonnerre@gmail.com>
2022-03-09 16:05:51 +0100

    :sparkles: can subscribe

--- src/pages/Video/Video.tsx
+++ src/pages/Video/Video.tsx
@@ -11,6 +11,7 @@ import dislike from '../../img/icons/dislike.png';
 import share from '../../img/icons/share.png';
 import download from '../../img/icons/download.png';
 import more from '../../img/icons/more.png';
+import { authenticatedFetch } from '../../utils';
 
 
 function Video() {
@@ -26,13 +27,28 @@ function Video() {
     })
 
     const [user, setUser] = React.useState({
+        id: '',
         name: '',
         photo: '',
         subscriber: '',
     })
+
+    const [sub, setSub] = React.useState(false)
     let { id } = useParams();
 
-    React.useEffect(() => {
+    const subscribe = (e: any) => {
+        const data = {
+            user_id: e,
+            subscriber_id: localStorage.userId
+        }
+        setSub(!sub)
+        authenticatedFetch('POST', `/subscribe`, data)
+            .then(res => {
+                getVideo()
+            })
+    }
+
+    const getVideo = () => {
         axios.get(`${url}/video/${id}`)
             .then((res) => {
                 let dataVideo = res.data[0]
@@ -48,12 +64,29 @@ function Video() {
                     created_at: dataVideo.created_at,
                 })
                 setUser({
+                    id: dataUser.id,
                     name: dataUser.name,
                     photo: dataUser.photo,
                     subscriber: dataUser.subscriber,
 
                 });
             })
+    }
+    const checkSub = () => {
+        const data = {
+            id: id,
+            subscriber: localStorage.userId
+        }
+        axios.get(`${url}/check-sub/${id}&${localStorage.userId}`)
+            .then((res) => {
+                setSub(res.data)
+
+            })
+    }
+
+    React.useEffect(() => {
+        getVideo()
+        checkSub()
     }, []);
     return (
         <section className='video-suggestion-container'>
@@ -100,7 +133,11 @@ function Video() {
                         </div>
                         <div className='subscribe-join-container'>
                             <button className='join'>REJOINDRE</button>
-                            <button className='subscribe'>S'ABONNER</button>
+                            {!sub ?
+                                <button onClick={() => subscribe(user.id)} className='subscribe'>S'ABONNER</button>
+                                :
+                                <button onClick={() => subscribe(user.id)} className='subscribe active'>ABONNÉ</button>
+                            }
                         </div>
                     </div>
 
