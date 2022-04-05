e5a5be5cecf208ec7bec7143f285c0e86376222e
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2022-04-05 10:38:34 +0100

    pull auto server

--- src/components/LateralNavbar/LateralNavbar.tsx
+++ src/components/LateralNavbar/LateralNavbar.tsx
@@ -12,7 +12,7 @@ function LateralNavbar() {
                 <Link className='text-decoration-none-link' to="/">
                     <div className='alignement-logo'>
                         <img src={logo_home} className="logo_lateral" alt="Home youbetu" />
-                        <p> Accueil aaaaaaaaaaaaaaaaaaa </p>
+                        <p> Accueil </p>
                     </div>
                 </Link>
                 <Link className='text-decoration-none-link' to="/">
